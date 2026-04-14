68eca63106259b1fec2b643e31e90dcc061679e3
iipthelp-sys <iipthelp@gmail.com>
2026-04-14 14:05:20 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,8 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-const debug = document.getElementById('debug')
-if (debug) debug.innerHTML += '<br>✅ main.jsx loaded'
-
-try {
-  ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  )
-  if (debug) debug.innerHTML += '<br> YES React mounted'
-} catch(e) {
-  if (debug) debug.innerHTML += '<br>❌ React error: ' + e.message
-  console.error(e)
-}
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
